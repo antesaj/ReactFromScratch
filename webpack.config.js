d194b084c13842ae7985e70aa60dcacb4fa28d70
Andrew Antes <antesaj@gmail.com>
2021-11-03 17:03:23 -0400

    More specific about name of entry

--- webpack.config.js
+++ webpack.config.js
@@ -2,11 +2,14 @@ const path = require("path");
 const webpack = require("webpack");
 
 module.exports = {
-    entry: "./src/index.js", // entrypoint for app, where we use React renderer
+    //entry: "./src/index.js", // entrypoint for app, where we use React renderer
+    entry: {
+        name: "./src/index.js"
+    },
     mode: "development", // development | production
     // inform webpack where to put bundled code + name of file
     output: {
-        filename: "bundle.js",
+        filename: "[name].[contenthash].js",
         path: path.resolve("dist"),
         publicPath: "/",
     },
